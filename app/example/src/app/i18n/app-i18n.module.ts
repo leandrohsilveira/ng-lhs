bf6e713c775643039b6e79ffb87c718c08bef9d9
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2021-09-21 11:10:15 -0300

    Remove unused pipe

--- app/example/src/app/i18n/app-i18n.module.ts
+++ app/example/src/app/i18n/app-i18n.module.ts
@@ -1,12 +1,11 @@
 import {NgModule} from '@angular/core'
-import {CommonModule, DatePipe} from '@angular/common'
+import {CommonModule} from '@angular/common'
 import {
   FIELD_I18N_SERVICE,
   FIELD_SUGGESTION_I18N_SERVICE,
 } from '@ngx-lhs/components'
 import {AppFieldI18nService} from './app-field-i18n.service'
 import {AppFieldSuggestionI18nService} from './app-field-suggestion-i18n.service'
-import {DateOnlyPipe} from './date-only.pipe'
 
 @NgModule({
   imports: [CommonModule],
@@ -19,9 +18,8 @@ import {DateOnlyPipe} from './date-only.pipe'
       provide: FIELD_SUGGESTION_I18N_SERVICE,
       useClass: AppFieldSuggestionI18nService,
     },
-    DatePipe,
   ],
-  declarations: [DateOnlyPipe],
-  exports: [DateOnlyPipe],
+  declarations: [],
+  exports: [],
 })
 export class AppI18nModule {}
