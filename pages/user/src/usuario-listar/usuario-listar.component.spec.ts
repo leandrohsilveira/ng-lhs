7681b568373fc387eaf2661ec9aa016df840fc46
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2021-09-22 22:51:24 -0300

    Add testing support

--- pages/user/src/usuario-listar/usuario-listar.component.spec.ts
+++ pages/user/src/usuario-listar/usuario-listar.component.spec.ts
@@ -1,6 +1,9 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing'
-
+import {UsuarioListarModule} from './usuario-listar.module'
 import {UsuarioListarComponent} from './usuario-listar.component'
+import {ListRouterService} from '@ngx-lhs/components'
+import {PapelService, UsuarioService} from '@app-modules/user'
+import {RouterModule} from '@angular/router'
 
 describe('UsuarioListarComponent', () => {
   let component: UsuarioListarComponent
@@ -8,11 +11,9 @@ describe('UsuarioListarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UsuarioListarComponent],
+      imports: [UsuarioListarModule, RouterModule.forRoot([])],
+      providers: [ListRouterService, PapelService, UsuarioService],
     }).compileComponents()
-  })
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(UsuarioListarComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
